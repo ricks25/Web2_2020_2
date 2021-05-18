b554863f14e4da785453a7bacf052991afada9c9
ricks25 <rick-fabris@hotmail.com>
2021-05-18 15:08:52 -0300

    Primeiro commit
    Adicionando tela inicial de cadastro de musicas, pagina de busca e login( ainda sem o banco de dados), adicioando uso inicial de cookies.

---
+++ model/Musicas.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports = class Musicas {
+    static async find(nomeMusica) {
+        let resultado;
+        const conn  = await MongoClient.connect('mongodb://localhost:27017/web2');
+        const db = conn.db();
+        reultado = await db.collection('musicas').find({ title: new RegExp('^' + nomeMusica) }).toArray();
+        conn.close();
+        return resultado;
+    }
+
+    static async insert(musica){
+        const conn  = await MongoClient.connect('mongodb://localhost:27017/web2');
+        const db = conn.db();
+        db.collection('musicas').insertOne({title: musica.title, band: musica.band});
+        conn.close();
+    }
+}
---
+++ server.js
@@ -0,0 +1,58 @@
+let http = require('http'),
+    path = require('path'),
+    express = require('express'),
+    cookieParser = require('cookie-parser'),
+    app = express(),
+    Musicas = require('./model/Musicas');
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'view'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.get('/', function (req, res, next) {
+    if(req.cookies && req.cookies.login) {
+        res.render('cadastroMusica');
+        return ;
+    }
+    res.redirect('login');
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/busca', async (req, res) => {
+    const musicas = await Musicas.find(req.query.busca);
+    res.render('paginaDeBusca', { musicas: musicas})
+})
+
+app.get('/cadastro', (req, res) => {
+    res.render('cadastroMusica');
+})
+
+app.post('/cadastro', async (req, res) => {
+    const musica = {
+        title: req.body.title,
+        band: req.body.band
+    }
+    Musicas.insert(musica);
+    res.end();
+})
+
+app.post('/login', (req, res) => {
+    let username = req.body.username,
+        senha = req.body.senha;
+    if(username === 'teste' && senha === 'teste123') {
+        res.cookie('login', 'teste');
+        res.redirect('/');
+        return ;
+    }else{
+        res.status(403);
+        res.write('<h1>Não foi possivel entrar!</h1>');
+        res.end();
+    }
+})
+
+app.listen(3000);
